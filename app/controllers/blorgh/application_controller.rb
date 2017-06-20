ab52d6f2e8c5a4fcef22bc651d3b1497f11da072
bocai <bocai@zhulux.local>
2017-06-20 15:14:46 +0800

    add some new function to host application

--- app/controllers/blorgh/application_controller.rb
+++ app/controllers/blorgh/application_controller.rb
@@ -1,8 +1,8 @@
 # make sure to load Blorgh::ApplicationController instead of the top ApplicationController
-require_dependency 'blorgh/application_controller' 
+#require_dependency 'blorgh/application_controller' 
 
 module Blorgh
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
   end
 end
